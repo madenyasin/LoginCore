7498ee49d97c119c7e708b79be3b5f3bb1cfba11
Yasin Maden <maden.yasin@hotmail.com>
2024-10-18 15:25:26 +0300

    Inception

---
+++ app/src/main/java/com/yasinmaden/logincore/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.yasinmaden.logincore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.yasinmaden.logincore.ui.theme.LoginCoreTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginCoreTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        TODO("Starting point of the app navigation")
+                    }
+                }
+            }
+        }
+    }
+}
