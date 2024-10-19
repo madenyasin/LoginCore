929582122e38fcfe021af2486c0ecc2cb484467d
Yasin Maden <maden.yasin@hotmail.com>
2024-10-19 23:59:09 +0300

    Added: Navigation structure to MainActivity.kt

--- app/src/main/java/com/yasinmaden/logincore/MainActivity.kt
+++ app/src/main/java/com/yasinmaden/logincore/MainActivity.kt
@@ -8,7 +8,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.yasinmaden.logincore.navigation.AppNavGraph
+import com.yasinmaden.logincore.ui.login.LoginScreen
+import com.yasinmaden.logincore.ui.login.LoginViewModel
 import com.yasinmaden.logincore.ui.theme.LoginCoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +24,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoginCoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        TODO("Starting point of the app navigation")
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        AppNavGraph(navController = rememberNavController())
                     }
                 }
             }
