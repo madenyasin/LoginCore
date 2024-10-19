3bdeb14a2c8598a66b534a27c5b28ae82c629d9d
Yasin Maden <maden.yasin@hotmail.com>
2024-10-19 23:49:46 +0300

    Added NavHost structure

---
+++ app/src/main/java/com/yasinmaden/logincore/navigation/AppNavGraph.kt
@@ -0,0 +1,37 @@
+package com.yasinmaden.logincore.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.yasinmaden.logincore.ui.login.LoginScreen
+import com.yasinmaden.logincore.ui.login.LoginViewModel
+import com.yasinmaden.logincore.ui.signup.SignUpScreen
+
+@Composable
+fun AppNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Destinations.Login.route
+    ) {
+        composable(route = Destinations.Login.route) {
+            val viewModel: LoginViewModel = viewModel()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+            val onAction = viewModel::onAction
+            val uiEffect = viewModel.uiEffect
+            LoginScreen(
+                uiState = uiState,
+                onAction = onAction,
+                uiEffect = uiEffect,
+                navController = navController
+            )
+        }
+        composable(route = Destinations.SignUp.route) {
+            SignUpScreen()
+        }
+    }
+}
+
