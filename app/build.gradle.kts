fd284486637cb28e8cf87e5156869672c43956f1
Yasin Maden <maden.yasin@hotmail.com>
2024-10-19 23:54:56 +0300

    Added: Navigation dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose.android)
 
+    implementation(libs.androidx.navigation.compose)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
