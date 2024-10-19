1375cb3711127bf8c1032144f5a198abfc04df97
Yasin Maden <maden.yasin@hotmail.com>
2024-10-20 00:05:09 +0300

    Optimized: Unused imports in MainActivity.kt

--- app/src/main/java/com/yasinmaden/logincore/MainActivity.kt
+++ app/src/main/java/com/yasinmaden/logincore/MainActivity.kt
@@ -8,14 +8,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.yasinmaden.logincore.navigation.AppNavGraph
-import com.yasinmaden.logincore.ui.login.LoginScreen
-import com.yasinmaden.logincore.ui.login.LoginViewModel
 import com.yasinmaden.logincore.ui.theme.LoginCoreTheme
 
 class MainActivity : ComponentActivity() {
