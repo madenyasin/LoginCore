37d642c3831d60f8d21640ed122ca68fc10c77be
Yasin Maden <maden.yasin@hotmail.com>
2024-10-20 17:56:19 +0300

    Created: Basic skeleton of SignUpScreen, SignUpViewModel and SignUpContract.

--- app/src/main/java/com/yasinmaden/logincore/ui/signup/SignUpContract.kt
+++ app/src/main/java/com/yasinmaden/logincore/ui/signup/SignUpContract.kt
@@ -2,15 +2,33 @@ package com.yasinmaden.logincore.ui.signup
 
 object SignUpContract {
     data class UiState(
+        val name: String = "",
         val email: String = "",
         val password: String = "",
         val confirmPassword: String = "",
+        val passwordVisibility: Boolean = false,
+        val confirmPasswordVisibility: Boolean = false
     )
 
-    sealed class UiAction(){
-        data class EmailChanged(val email: String): UiAction()
+    sealed class UiAction() {
+        data class OnNameChange(val name: String) : UiAction()
+        data class OnEmailChange(val email: String) : UiAction()
+        data class OnPasswordChange(val password: String) : UiAction()
+        data class OnConfirmPasswordChange(val confirmPassword: String) : UiAction()
+        data class OnSignUpClick(
+            val name: String,
+            val email: String,
+            val password: String,
+            val confirmPassword: String
+        ) : UiAction()
+
+        object OnSignInTextClick: UiAction()
+        object OnPasswordVisibilityChange : UiAction()
+        object OnConfirmPasswordVisibilityChange : UiAction()
 
     }
 
-    sealed class UiEffect()
+    sealed class UiEffect() {
+        object OnNavigateToLoginScreen : UiEffect()
+    }
 }
