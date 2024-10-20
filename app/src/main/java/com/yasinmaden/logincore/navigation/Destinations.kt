e21461c7e75f946c5b80d6d3cca9ecb4c6b518e6
Yasin Maden <maden.yasin@hotmail.com>
2024-10-20 17:52:42 +0300

    Updated: Data objects in UI contract to objects.

--- app/src/main/java/com/yasinmaden/logincore/navigation/Destinations.kt
+++ app/src/main/java/com/yasinmaden/logincore/navigation/Destinations.kt
@@ -1,6 +1,6 @@
 package com.yasinmaden.logincore.navigation
 
 sealed class Destinations(val route: String) {
-    data object Login : Destinations("login")
-    data object SignUp : Destinations("sign_up")
+    object Login : Destinations("login")
+    object SignUp : Destinations("sign_up")
 }
