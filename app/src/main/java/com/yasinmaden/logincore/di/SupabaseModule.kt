ca84f2c12a650826f4963c79a139eb87b5f88c93
Yasin Maden <maden.yasin@hotmail.com>
2024-11-28 19:01:39 +0300

    feat: Created Supabase module

---
+++ app/src/main/java/com/yasinmaden/logincore/di/SupabaseModule.kt
@@ -0,0 +1,43 @@
+package com.yasinmaden.logincore.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.postgrest
+import io.github.jan.supabase.storage.Storage
+import io.github.jan.supabase.storage.storage
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SupabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideSupabaseClient(): SupabaseClient {
+        return createSupabaseClient(
+            supabaseUrl = "",
+            supabaseKey = ""
+        ) {
+            install(Postgrest)
+            install(Storage)
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideSupabaseDatabase(client: SupabaseClient): Postgrest {
+        return client.postgrest
+    }
+
+    @Provides
+    @Singleton
+    fun provideSupabaseStorage(client: SupabaseClient): Storage {
+        return client.storage
+    }
+
+}
